b6370f529fb8e3ecdd289de01ceb4ed5d277b329
yeremeee <152689136+yeremeee@users.noreply.github.com>
2026-09-17 14:05:25 +0900

    [level 0] Title: 더 크게 합치기, Time: 5.03 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        int ab = Integer.parseInt("" + a + b);
+        int ba = Integer.parseInt(""+ b + a);
+        
+        if (ab >= ba){
+            answer = ab;
+        } else {
+            answer = ba;
+        }
+        
+        return answer;
+    }
+}
