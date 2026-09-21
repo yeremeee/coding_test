8ee9373dd50d1db696b69a17e2c86d1b384a111d
yeremeee <152689136+yeremeee@users.noreply.github.com>
2026-09-21 13:18:30 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.41 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -4,13 +4,11 @@ class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
         List<Integer> rank = new ArrayList<>();
-        int rankCount = 0;
         
         for (int i = 0; i < score.length; i++){
             if (rank.size() < k){
                 rank.add(score[i]);
-                rankCount++;
-            } else if (rankCount == k && Collections.min(rank) < score[i]){
+            } else if (rank.size() == k && Collections.min(rank) < score[i]){
                 rank.remove(Collections.min(rank));
                 rank.add(score[i]);
             }
