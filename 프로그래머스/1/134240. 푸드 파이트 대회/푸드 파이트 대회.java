36994868f5edd99b54de77f1d7b3d3ad5ee393bc
yeremeee <152689136+yeremeee@users.noreply.github.com>
2026-09-22 12:05:06 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 3.98 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i < food.length; i++){
+            sb.append(String.valueOf(i).repeat(food[i] / 2));
+        }
+        
+        answer = sb.toString() + "0" + sb.reverse().toString();
+        
+        return answer;
+    }
+}
